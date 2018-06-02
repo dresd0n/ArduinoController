0efea812d9259645ef5163f40c8a06b7da3acd5b
dresd0n <dresd0n@users.noreply.github.com>
2018-06-01 23:18:46 -0500

    Updated the code with a new path name and, more importantly, added code to successfully modify the font type, style and size.

--- src/BTRobotMover.java
+++ src/BTRobotMover.java
@@ -23,7 +23,7 @@ public class BTRobotMover extends JPanel
              
                                           FocusListener {
     Arduino robot;
-	String csvDir = "C:\\Users\\dresdon\\Documents\\APCSP\\FinalProject2018\\";
+	String csvDir = "D:\\Users\\Don\\Documents\\APCSP\\FinalProject2018\\";
     JTextField dirField, fileField;
     JSpinner portSpinner;
     boolean inputSet = false;
@@ -414,8 +414,19 @@ public class BTRobotMover extends JPanel
         frame.pack();
         frame.setVisible(true);
     }
+    
+    public static void setUIFont(javax.swing.plaf.FontUIResource f) {
+    	java.util.Enumeration keys = UIManager.getDefaults().keys();
+    	while (keys.hasMoreElements()) {
+    		Object key = keys.nextElement();
+    		Object value = UIManager.get(key);
+    		if (value instanceof javax.swing.plaf.FontUIResource)
+    			UIManager.put(key, f);
+    	}
+    }
 
     public static void main(String[] args) {
+    	setUIFont (new javax.swing.plaf.FontUIResource("Verdana", Font.BOLD, 22));
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
